057dddd29f9204c2958f5f478dbd9e1877ac8ef3
Markos Yquires <markosyquires@gmail.com>
2021-01-23 00:56:49 +0400

    cool

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,31 @@
 mod common;
-
 mod public;
-// use crate::public::*;
-
 mod windows;
 
-use crate::windows::*;
-
 use crate::public::KeybdKey::*;
+use crate::windows::*;
 use std::{thread::sleep, time::Duration};
 
 fn main() {
     listener()
 }
 
+// ---------------------------------------------------------------------------
 fn listener() {
-    SpaceKey.bind(|| {
-        QKey.press();
-        sleep(Duration::from_millis(10));
-        QKey.release();
-        WKey.press();
-        sleep(Duration::from_millis(10));
-        WKey.release();
-        EKey.press();
-        sleep(Duration::from_millis(10));
-        EKey.release();
-        RKey.press();
-        sleep(Duration::from_millis(10));
-        RKey.release();
-    });
-
-    // Call this to start listening for bound inputs.
+    SpaceKey.bind(|| generated_cb_function(vec![1, 1, 1, 0, 2, 3, 4]));
     handle_input_events();
 }
+
+fn generated_cb_function(nums: Vec<usize>) {
+    let arr = [QKey, WKey, EKey, RKey, IKey];
+    for (i, num) in nums.iter().enumerate() {
+        if nums[i] < arr.len() {
+            let key = arr[*num as usize];
+            key.press();
+            sleep(Duration::from_millis(10));
+            key.release();
+        }
+    }
+    // }
+}
+// ----------------------------------------------------------------------------
